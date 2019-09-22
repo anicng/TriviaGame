cd7b9aee3fa2e01cb81b6cbdca5acf9f3b931298
Annie <annie.yiyunc@gmail.com>
2019-09-22 14:04:35 -0700

    added timer to question page

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,31 @@
 $(document).ready(
     function triviaGame() {
-       
+        var number = 60;
+        var intervalId;
+
+        function decrement() {
+            number--;
+            $("#time-countdown-text").text(number);
+            if (number === 0) {
+                stop();
+            }
+        };
+
+        function run() {
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
+        };
+        run();
+        console.log(intervalId);
+
+        function stop() {
+            clearInterval(intervalId);
+        };
+
         $("#startButton").click(function () {
             window.location.href = "index.html";
+            run();
+            console.log(number);
+            console.log(intervalId);
         });
     });
