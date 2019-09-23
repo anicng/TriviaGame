4ca41c064401f99e6a7b9b1c685008b220ef1572
Annie <annie.yiyunc@gmail.com>
2019-09-22 19:37:33 -0700

    when click on an answer in question1 return correct or incorrect value

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,9 @@
 $(document).ready(
     function triviaGame() {
-        var number = 60;
+        var number = 3;
         var intervalId;
 
+        // Timer decrement
         function decrement() {
             number--;
             $("#time-countdown-text").text(number);
@@ -11,21 +12,47 @@ $(document).ready(
             }
         };
 
+        // Run timer
         function run() {
             clearInterval(intervalId);
             intervalId = setInterval(decrement, 1000);
         };
         run();
-        console.log(intervalId);
 
+        // Timer stop
         function stop() {
             clearInterval(intervalId);
         };
 
+        // When click on startButton, go to question page and start timer
         $("#startButton").click(function () {
             window.location.href = "index.html";
             run();
             console.log(number);
             console.log(intervalId);
         });
+
+        var correctCount = 0;
+        var incorrectCount = 0;
+
+
+        // function selectedRadioValue() {
+        //     var radioValue = $("input").attr("value");
+        //     console.log(radioValue);
+        //     if (radioValue === "correct") {
+        //         correctCount++;
+        //         $("correctAnswers").text(correctCount);
+        //     }
+        // };
+
+        function selectedRadioValue() {
+            var radioValue = $("input[type=radio][name=question1]:checked").val();
+            console.log(radioValue);
+        }
+
+        $("input").on("click", function () {
+            selectedRadioValue();
+        });
+
+
     });
